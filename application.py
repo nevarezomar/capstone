d482cfe5a9fa896d8c1afe6b2dccb7bc1991b929
Omar Nevarez <nevarezomar93@gmail.com>
2022-01-28 11:02:46 -0800

    Added templates and bootstrap

--- application.py
+++ application.py
@@ -1,15 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 application = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Omar Nevarez',
+        'title': 'Blog Post 1',
+        'content': 'First posted content',
+        'date_posted': 'January 28, 2022'
+    },
+    {
+        'author': 'Not Omar',
+        'title': 'Blog Post 2',
+        'content': 'Second posted content',
+        'date_posted': 'January 29, 2022'
+    }
+]
+
+
 
 @application.route("/")
 @application.route("/home")
 def hello():
-    return "<h1>Hello Home!</h1>"
+    return render_template('home.html', posts=posts)
 
 @application.route("/about")
 def about():
-    return "<h1>About Page!</h1>"
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     application.run(debug = True)
