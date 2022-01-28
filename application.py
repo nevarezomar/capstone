0998faf7506320199cab3eb561628e1c47afb464
Omar Nevarez <nevarezomar93@gmail.com>
2022-01-28 10:08:47 -0800

    setup

--- application.py
+++ application.py
@@ -1,1 +1,15 @@
 from flask import Flask
+
+application = Flask(__name__)
+
+@application.route("/")
+@application.route("/home")
+def hello():
+    return "<h1>Hello Home!</h1>"
+
+@application.route("/about")
+def about():
+    return "<h1>About Page!</h1>"
+
+if __name__ == '__main__':
+    application.run(debug = True)
