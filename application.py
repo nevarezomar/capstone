1218a16aaff56040d723edfa578cc6df56407f8e
nevarezomar <nevarezomar93@gmail.com>
2022-02-01 12:25:34 -0800

    Added sign-up and login pages

--- application.py
+++ application.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 application = Flask(__name__)
+
+application.config['SECRET_KEY'] = '9fe2cf6f218f9017cb0f237ec1621555'
 
 posts = [
     {
         'author': 'Omar Nevarez',
-        'title': 'Blog Post 1',
+        'title': 'Workout 1',
         'content': 'First posted content',
         'date_posted': 'January 28, 2022'
     },
     {
         'author': 'Not Omar',
-        'title': 'Blog Post 2',
+        'title': 'Workout 2',
         'content': 'Second posted content',
         'date_posted': 'January 29, 2022'
     }
@@ -27,6 +30,27 @@ def hello():
 @application.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@application.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@application.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
 
 if __name__ == '__main__':
     application.run(debug = True)
