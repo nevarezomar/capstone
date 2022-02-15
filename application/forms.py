9a65d7d10449cd08c00dd13b741648353b43aa9b
nevarezomar <nevarezomar93@gmail.com>
2022-02-15 11:24:37 -0800

    Users can create new workouts

--- application/forms.py
+++ application/forms.py
@@ -1,7 +1,8 @@
+from typing import Text
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from application.models import User
 
@@ -53,3 +54,8 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('Email already in use!')
+
+class PostFrom(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
--- application/routes.py
+++ application/routes.py
@@ -3,30 +3,15 @@ import os
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from application import application, db, bcrypt
-from application.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from application.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostFrom
 from application.models import User, Workout
 from flask_login import login_user, current_user, logout_user, login_required
-
-posts = [
-    {
-        'author': 'Omar Nevarez',
-        'title': 'Workout 1',
-        'content': 'First posted content',
-        'date_posted': 'January 28, 2022'
-    },
-    {
-        'author': 'Not Omar',
-        'title': 'Workout 2',
-        'content': 'Second posted content',
-        'date_posted': 'January 29, 2022'
-    }
-]
-
 
 
 @application.route("/")
 @application.route("/home")
 def home():
+    posts = Workout.query.all()
     return render_template('home.html', posts=posts)
 
 @application.route("/about")
@@ -72,7 +57,7 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(application.root_path, 'static/profile_pics', picture_fn)
-    
+
     output_size = (125, 125)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
@@ -98,3 +83,15 @@ def account():
     
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+@application.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostFrom()
+    if form.validate_on_submit():
+        post = Workout(title=form.title.data, content=form.content.data, member=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('new_post.html', title='New Post', form=form)
